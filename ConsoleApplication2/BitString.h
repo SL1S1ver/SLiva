2092c597152b1e15b0c4a64c7b5a34d580be7606
SL1S1ver <slivaroman2004@mail.ru>
2026-03-27 09:39:53 +0600

    Add files via upload

---
+++ ConsoleApplication2/BitString.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <string>
+using namespace std;
+
+class BitString
+{
+    unsigned char bs[8];
+
+public:
+    BitString();
+    BitString(string inp_string);
+    void input();
+    void output();
+    BitString conjunction(BitString b);
+    void fileoutput(string filename);
+    void fileinput(string filename);
+}; 
